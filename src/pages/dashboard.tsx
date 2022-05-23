e2aed211ad06e48ab3b9c78d846f3199d1c491a1
Helder Correia <helder.patrick25@gmail.com>
2022-05-23 10:00:40 +0100

    Break Header, Sidebar and Pagination in components

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,56 +2,11 @@ import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import dynamic from 'next/dynamic';
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-
+import {options, series} from "../../utils/chartData";
 const Chart = dynamic(() => import('react-apexcharts'), {
   ssr: false,
 });
 
-const options = {
-  chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[500]
-  },
-  grid: {
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-
-  xaxis: {
-    type: "datetime" as const,
-    axisBorder: {
-        color: theme.colors.gray[600],
-    },
-    axisTicks: {
-        color: theme.colors.gray[600],
-    },
-    categories: ['2022-05-18T00:00:00.000Z', '2022-05-19T00:00:00.000Z', '2022-05-20T00:00:00.000Z', '2022-05-21T00:00:00.000Z', '2022-05-22T00:00:00.000Z', '2022-05-23T00:00:00.000Z', '2022-05-24T00:00:00.000Z']
-  },
-  
-  fill: {
-    opacity: 0.3,
-    type: 'gradient',
-    gradient: {
-      shade: 'dark',
-      opacityFrom: 0.7,
-      opacityTo: 0.3,
-    }
-  }
-};
-
-const series = [
-  { name: 'series1', data: [31, 120, 10, 28, 51, 18, 109] }
-];
 
 export default function Dashboard() {
   return(
