8cf2e574d4854214d983bf8ad89b20ec72a75d2a
Helder Correia <helder.patrick25@gmail.com>
2022-06-06 08:27:42 +0100

    setup axios to the project

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,6 +8,7 @@ import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 import useSSR from '../../hooks/useSSR';
+import { api } from "../../services/api";
 
 export default function UserList() {
 
@@ -19,8 +20,7 @@ export default function UserList() {
   })
 
   const { data, isLoading, error, isFetching } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+    const { data } = await api.get('/users');
 
     const users = data.users.map(user => ({
       id: user.id,
